7f915f4a78c48eb9056ebf46d612ff15940db335
Alin Bonat <alin.bonat@gmail.com>
2017-07-07 00:15:06 +0300

    Finalizare homepage (cele mai recente consultatii adaugate)

--- index.php
+++ index.php
@@ -13,6 +13,11 @@ $nrTotal = $row["NrTotal"];
 $nrTotalBarbati = $row["NrTotalBarbati"];
 $nrTotalFemei = $row["NrTotalFemei"];
 
+$sqlConsultatiiRecente = "SELECT c.`id`, p.`nume`, p.`prenume`, c.`ritm_cardiac`, c.`ritm_respirator`, c.`tensiune_sis`, c.`tensiune_dia`, c.`temperatura`, c.`observatii`, c.`adaugat` "
+        . "FROM `consultatii` c "
+        . "INNER JOIN `persoane` p ON c.`idPersoana`=p.`id` ORDER BY c.`id` DESC LIMIT 8";
+$query = mysqli_query($con, $sqlConsultatiiRecente);
+
 ?>
 <div id="page-wrapper">
     <div class="row">
@@ -79,7 +84,40 @@ $nrTotalFemei = $row["NrTotalFemei"];
         </div>
     </div>
     <div class="container">
-        
+        <div class="col-lg-12">
+            <div class="panel panel-primary">
+                <div class="panel-heading " style="color: #FFF; font-size: 18px;">
+                    Cele mai recente consultatii adaugate <i class="fa fa-address-book" aria-hidden="true"></i>
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-condensed table-hover" style="margin-bottom: 0px;">
+                            <thead>
+                                <tr><th>Nr. Crt.</th><th>Nume</th><th>Prenume</th><th>Ritm Cardiac</th><th>Ritm Respirator</th><th>Tensiune Arteriala</th><th>Temperatura</th><th style="width:390px">Observatii</th><th>Data</th></tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                $counter = 1;
+                                while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
+                                    echo '<tr>'
+                                            .'<td>'.$counter.'</td>'
+                                            .'<td>'.$row["nume"].'</td>'
+                                            .'<td>'.$row["prenume"].'</td>'
+                                            .'<td>'.$row["ritm_cardiac"].'</td>'
+                                            .'<td>'.$row["ritm_respirator"].'</td>'
+                                            .'<td>'.$row["tensiune_sis"].' / '.$row["tensiune_dia"].'</td>'
+                                            .'<td>'.$row["temperatura"].'</td>'
+                                            .'<td>'.$row["observatii"].'</td>'
+                                            .'<td>'.$row["adaugat"].'</td>'
+                                        .'</tr>';
+                                    $counter++;
+                                }?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
